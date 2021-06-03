128555cad7588f0589669efd3939646858cb46de
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-06-03 17:10:43 -0400

    Set up dictionary form
    Set up dictionary form for user to submit a word.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,16 +2,24 @@ import React, { useState } from "react";
 
 export default function Dictionary() {
 
-    let
+    let [keyword, setKeyword] = useState("");
 
+    function handleKeyword(event) {
+        console.log(event);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+    }
+ 
     return (
-        <form className="Dictionary-form">
+        <form className="Dictionary-form" onSubmit={handleSubmit}>
 
-            <input type="text" autofocus="on"
+            <input type="search" autoFocus="on"
             placeholder="Enter a word...">
             </input>
 
-            <input type="submit"></input>
+            <input type="submit" onChange={handleKeyword}></input>
 
         </form>
     )
