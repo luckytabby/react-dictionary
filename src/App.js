cdd3500f8e477e4446ada069493f81ba7175eaa9
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-06-03 16:48:07 -0400

    Created Dictionary.js component
    Created Dictionary.js component, which will make API calls and return definition data.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Dictionary from "./Dictionary"
 
 function App() {
   return (
     <div className="App">
+
+      <h1 className="App-header">
+        Digital Dictionary
+      </h1>
+
+      <Dictionary />
 
     </div>
   );
---
+++ src/Dictionary.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+
+    let
+
+    return (
+        <form className="Dictionary-form">
+
+            <input type="text" autofocus="on"
+            placeholder="Enter a word...">
+            </input>
+
+            <input type="submit"></input>
+
+        </form>
+    )
+}
