3033003590a4b6478c48b0479dac2024b691c386
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-06-07 16:38:57 -0400

    Created Phonetics.js
    Phonetics.js will house IPA text and pronunciation recording.

---
+++ src/Phonetics.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Phonetics(props) {
+    if (props.phonetics) {
+        console.log(props.phonetics);
+        return null;
+    }
+    else {
+        return null;
+    }
+}
--- src/Response.js
+++ src/Response.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Response(props) {
 
@@ -7,6 +8,7 @@ export default function Response(props) {
         return (
             <div className="Response">
                 <h2>{props.response.word}</h2>
+                <Phonetics phonetics={props.response.phonetics} />
                 {props.response.meanings.map(function (meaning, index) {
                     return (
                         <div key={index}>
