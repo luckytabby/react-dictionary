c3ae3904e95970c013c454b08ad02d1d21e37b5a
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-06-09 11:40:28 -0400

    Updated signature and styling

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ function App() {
   return (
     <div className="App">
 
-      <h1 className="App-header">
-        Digital Dictionary
-      </h1>
+      <div className="App-header">
 
-      <Dictionary defaultKeyword="yeehaw" />
+        <h1 className="App-heading">
+          Digital Dictionary
+        </h1>
 
-      <p className="App-signature">
-          This project is <a href="https://github.com/maggiegmcd/react-dictionary" target="_blank" rel="noopener noreferrer">open-sourced</a> and 
-          coded with ♥︎ by <a href="https://www.linkedin.com/in/maggiegmcd/" target="_blank" rel="noopener noreferrer">Maggie McDonald</a>.
-      </p>
+       </div>
+
+       <div className="App-body">
+
+        <Dictionary defaultKeyword="yeehaw" />
+
+      </div>
+
+      <div className="App-footer">
+
+        <p className="App-signature">
+            A Pig in a Poke Studio production, built with <a href="https://github.com/maggiegmcd/react-dictionary" target="_blank" rel="noopener noreferrer">open-source code</a> and 
+            ♥︎ by <a href="https://www.linkedin.com/in/maggiegmcd/" target="_blank" rel="noopener noreferrer">Maggie McDonald</a>.
+        </p>
+
+      </div>
 
     </div>
   );
