c713209a1f24b780d1c578567e4c2ddf1b99e157
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-06-10 11:15:54 -0400

    Tinkered with custom font

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
 
         <h1 className="App-heading">
-          Digital Dictionary
+          DIGITAL DICTIONARY
         </h1>
 
         <Dictionary defaultKeyword="yeehaw" />
 
         <p className="App-signature">
-            A <a href="https://www.instagram.com/piginapokestudio/" target="_blank" rel="noopener noreferrer">Pig in a Poke Studio</a> production, built with <a href="https://github.com/maggiegmcd/react-dictionary" target="_blank" rel="noopener noreferrer">open-source code</a> and 
-            ♥︎ by <a href="https://www.linkedin.com/in/maggiegmcd/" target="_blank" rel="noopener noreferrer">Maggie McDonald</a>.
+            A <a href="https://www.instagram.com/piginapokestudio/" target="_blank" rel="noopener noreferrer">PIG IN A POKE STUDIO</a> PRODUCTION, BUILT WITH <a href="https://github.com/maggiegmcd/react-dictionary" target="_blank" rel="noopener noreferrer">OPEN-SOURCE CODE</a> AND 
+            ♥︎ BY <a href="https://www.linkedin.com/in/maggiegmcd/" target="_blank" rel="noopener noreferrer">MAGGIE MCDONALD</a>.
         </p>
 
     </div>
