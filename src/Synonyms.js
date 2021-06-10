506d076126c088f177163af62758c08ad7552720
Maggie McDonald <maggiegreymcdonald@gmail.com>
2021-06-10 09:24:14 -0400

    Updated/pared down font files

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -6,7 +6,7 @@ export default function Synonyms(props) {
             <ul className="Synonyms">
                 {props.synonyms.map(function(synonym, index) {
                     return (<li key={index}>
-                            Def: {synonym}
+                            {synonym}
                         </li>)
                 })}
             </ul>
